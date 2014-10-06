60b5965dc40668e6f124887d700536c2771e39a8
Rattraps <autumnkelty@gmail.com>
2014-10-06 17:07:12 -0400

    Update WallHit.py
    fixed it

--- WallHit.py
+++ WallHit.py
@@ -1,7 +1,8 @@
-robot = create.Create(4)
-sensors = robot.sensors([create.LEFT_BUMP, create.RIGHT_BUMP])
+import create
+robot = create.Create(3)
 robot.toFullMode()
 robot.go(100,0)
-while(true):
-	if (sensors[create.LEFT_BUMP] > 0 or [create.RIGHT_BUMP] > 0):
-		robot.stop()
+while(1 == 1):
+    sensors = robot.sensors([create.LEFT_BUMP, create.RIGHT_BUMP])
+    if (sensors[create.LEFT_BUMP] == 1) or (sensors[create.RIGHT_BUMP] == 1):
+        robot.stop()
