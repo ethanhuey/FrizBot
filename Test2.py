d705be0d24b6d4f03b91c74160a29b143e14fd16
Ethan Huey <ethanhuey@gmail.com>
2014-10-21 15:26:48 -0400

    Removed unnecessary files

--- Test2.py
+++
@@ -1,4 +0,0 @@
-import create
-r=create.Create(3)
-r.go(10,0)
-r.stop()
--- WallHit.py
+++
@@ -1,8 +0,0 @@
-import create
-robot = create.Create(3)
-robot.toFullMode()
-robot.go(100,0)
-while(1 == 1):
-    sensors = robot.sensors([create.LEFT_BUMP, create.RIGHT_BUMP])
-    if (sensors[create.LEFT_BUMP] == 1) or (sensors[create.RIGHT_BUMP] == 1):
-        robot.stop()
