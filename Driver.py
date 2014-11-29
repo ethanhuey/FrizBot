a24dea58aed9f53930be726239d9233cf40f5ee1
Rattraps <autumnkelty@gmail.com>
2014-11-29 17:14:07 -0500

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -15,7 +15,7 @@ class Processor:
 				if(current == "right"):
 					robot.turn(-90, 45)
 					robot.go(20,0)
-				if(current == "sing")
+				if(current == "sing"):
 					robot.playSong( [(88,8),(88,16),(88,16),(84,8),(88,16),(91,32),(79,16)] )
 	
 			else:
@@ -29,11 +29,11 @@ class Keeper:
 			sensors = robot.sensors([create.LEFT_BUMP, create.RIGHT_BUMP, create.CLIFF_FRONT_LEFT, create.CLIFF_FRONT_RIGHT, create.CLIFF_RIGHT, create.CLIFF_LEFT])
 			if (sensors[create.LEFT_BUMP] == 1) or (sensors[create.RIGHT_BUMP] == 1) or (sensors[create.CLIFF_FRONT_LEFT] == 1) or (sensors[create.CLIFF_FRONT_RIGHT] == 1) or (sensors[create.CLIFF_RIGHT] == 1) or (sensors[create.CLIFF_LEFT] == 1):
 				robot.stop()
-				timer = 20;
+				timer = 20
 			elif (timer == 0):
 				robot.stop()
-				timer = 20;
+				timer = 20
 			else:
-				timer--;
+				timer = timer - 1
 
 
