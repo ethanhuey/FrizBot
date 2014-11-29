9217838a2e3518ef252949892143d50b9f969252
Rattraps <autumnkelty@gmail.com>
2014-11-29 15:22:40 -0500

    Update and rename bleh.py to Driver.py

---
+++ Driver.py
@@ -0,0 +1,30 @@
+
+
+
+class Processor:
+	commands = ["left", "forward", "forward", "right"]
+	def process():
+		while True:
+			if(len(commands) != 0):
+				current = commands.pop(0)
+				if(current == "left"):
+					robot.turn(90, 45)
+					robot.go(20,0)
+				if(current == "forward"):
+					robot.go(20,0)
+				if(current == "right"):
+					robot.turn(-90, 45)
+					robot.go(20,0)
+	
+			else:
+				commands.append("sing")
+				
+
+class Keeper:
+	def keepsafe():
+		while True:
+			sensors = robot.sensors([create.LEFT_BUMP, create.RIGHT_BUMP, create.CLIFF_FRONT_LEFT, create.CLIFF_FRONT_RIGHT, create.CLIFF_RIGHT, create.CLIFF_LEFT])
+			if (sensors[create.LEFT_BUMP] == 1) or (sensors[create.RIGHT_BUMP] == 1) or (sensors[create.CLIFF_FRONT_LEFT] == 1) or (sensors[create.CLIFF_FRONT_RIGHT] == 1) or (sensors[create.CLIFF_RIGHT] == 1) or (sensors[create.CLIFF_LEFT] == 1):
+				robot.stop()
+
+
