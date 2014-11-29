e4f424e7f3ec8f50244a045c151cd0be8140f87b
Rattraps <autumnkelty@gmail.com>
2014-11-29 18:05:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import Netcom
 import Driver
 import threading
 
-r = create.Create(3)
-r.toFullMode()
 
 comm = Netcom.Netcom()
 proc = Driver.Processor()
@@ -13,9 +11,9 @@ keep = Driver.Keeper()
 
 comm.initComms("gyumii", "oauth:i3vra1n8554xvy4ox870rnhnx10cko")
 try:
-	thread1 = threading.Thread(None, proc.process(r), "Thread_1", (), {}) 
+	thread1 = threading.Thread(None, proc.process(), "Thread_1", (), {}) 
 	thread1.start()
-	thread2 = threading.Thread(None, keep.keepsafe(r), "Thread_2", (), {}) 
+	thread2 = threading.Thread(None, keep.keepsafe(), "Thread_2", (), {}) 
 	thread2.start()
 except:
 	print ("dang")
