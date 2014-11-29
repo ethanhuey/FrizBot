47301be4da158355d95d70ca2534499868a265db
Rattraps <autumnkelty@gmail.com>
2014-11-29 16:49:58 -0500

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -15,6 +15,8 @@ class Processor:
 				if(current == "right"):
 					robot.turn(-90, 45)
 					robot.go(20,0)
+				if(current == "sing")
+					robot.playSong( [(88,8),(88,16),(88,16),(84,8),(88,16),(91,32),(79,16)] )
 	
 			else:
 				commands.append("sing")
