99b09432ee1d7c15eec5f71656fccbbf2c67971e
Rattraps <autumnkelty@gmail.com>
2014-11-29 15:29:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import create
 import time
 import Netcom
+import Driver
 
-#PORT_NUMBER = 3 
-#PORT_NUMBER = "dev/tty.KeySerial1"
-
-#r = create.Create(PORT_NUMBER);
+r = create.Create(3)
+r.toFullMode()
 
 comm = Netcom.Netcom()
+proc = Driver.Processor()
+keep = Driver.Keeper()
+
 comm.initComms("gyumii", "oauth:i3vra1n8554xvy4ox870rnhnx10cko")
-#TODO
+try:
+	thread1 = threading.Thread(None, proc.process(), "Thread_1", (), {}) 
+	thread1.start()
+	thread2 = threading.Thread(None, keep.keepsafe(), "Thread_2", (), {}) 
+	thread2.start()
+except:
+	print ("dang")
