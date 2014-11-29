2716f96b2150530c56b5c84a8d97fd8a3446cf66
Rattraps <autumnkelty@gmail.com>
2014-11-29 16:18:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ keep = Driver.Keeper()
 
 comm.initComms("gyumii", "oauth:i3vra1n8554xvy4ox870rnhnx10cko")
 try:
-	thread1 = threading.Thread(None, proc.process(), "Thread_1", (), {}) 
+	thread1 = threading.Thread(None, proc.process(), "Thread_1", (r), {}) 
 	thread1.start()
-	thread2 = threading.Thread(None, keep.keepsafe(), "Thread_2", (), {}) 
+	thread2 = threading.Thread(None, keep.keepsafe(), "Thread_2", (r), {}) 
 	thread2.start()
 except:
 	print ("dang")
