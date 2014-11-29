a763af5e6f8535ffaaaf4ce03d7a91efb7390b83
Rattraps <autumnkelty@gmail.com>
2014-11-29 16:40:56 -0500

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -21,10 +21,17 @@ class Processor:
 				
 
 class Keeper:
+	timer = 20;
 	def keepsafe(robot):
 		while True:
 			sensors = robot.sensors([create.LEFT_BUMP, create.RIGHT_BUMP, create.CLIFF_FRONT_LEFT, create.CLIFF_FRONT_RIGHT, create.CLIFF_RIGHT, create.CLIFF_LEFT])
 			if (sensors[create.LEFT_BUMP] == 1) or (sensors[create.RIGHT_BUMP] == 1) or (sensors[create.CLIFF_FRONT_LEFT] == 1) or (sensors[create.CLIFF_FRONT_RIGHT] == 1) or (sensors[create.CLIFF_RIGHT] == 1) or (sensors[create.CLIFF_LEFT] == 1):
 				robot.stop()
+				timer = 20;
+			elif (timer == 0):
+				robot.stop()
+				timer = 20;
+			else:
+				timer--;
 
 
