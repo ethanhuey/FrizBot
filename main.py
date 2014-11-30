e8975bb9c639ff6e9fc059b974bbb5f7af276bcf
Rattraps <autumnkelty@gmail.com>
2014-11-29 19:31:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ r = create.Create(3)
 r.toFullMode()
 
 comm = Netcom.Netcom()
-proc = Driver.Processor()
-keep = Driver.Keeper()
 
 comm.initComms("gyumii", "oauth:i3vra1n8554xvy4ox870rnhnx10cko")
 
