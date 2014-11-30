55e1fc157acececec4d2cb92739d3febe5e7a662
ashleycrouch <ashley.crouch@rocketmail.com>
2014-11-29 21:30:13 -0500

    Update Driver.py
    ALL FIXED

--- Driver.py
+++ Driver.py
@@ -1,17 +1,16 @@
 
+
 class Processor:
 	def process(self, robot, current):
-		if(current == "left"):
+		if current == "left":
 			robot.turn(90, 45)
-			robot.go(20,0)
-		if(current == "move"):
-			robot.go(20,0)
-		if(current == "right"):
+			robot.move(50,25)
+		if current == "move":
+			robot.move(50,25)
+		if current == "right":
 			robot.turn(-90, 45)
-			robot.go(20,0)
-		if(current == "sing"):
+			robot.move(50,25)
+		if current == "sing":
 			robot.playSong( [(88,8),(88,16),(88,16),(84,8),(88,16),(91,32),(79,16)] )
-
-
 
 
