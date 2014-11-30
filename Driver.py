d825ac2e12fcab0bf17d2591f8ffbe9b3709d42d
Rattraps <autumnkelty@gmail.com>
2014-11-29 19:31:18 -0500

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -1,37 +1,17 @@
 
 class Processor:
-	commands = ["left", "forward", "forward", "right"]
-	def process(self, robot):
-		while True:
-			if(len(self.commands) != 0):
-				current = self.commands.pop(0)
-				if(current == "left"):
-					robot.turn(90, 45)
-					robot.go(20,0)
-				if(current == "forward"):
-					robot.go(20,0)
-				if(current == "right"):
-					robot.turn(-90, 45)
-					robot.go(20,0)
-				if(current == "sing"):
-					robot.playSong( [(88,8),(88,16),(88,16),(84,8),(88,16),(91,32),(79,16)] )
-	
-			else:
-				self.commands.append("sing")
-				
+	def process(self, robot, current):
+		if(current == "left"):
+			robot.turn(90, 45)
+			robot.go(20,0)
+		if(current == "move"):
+			robot.go(20,0)
+		if(current == "right"):
+			robot.turn(-90, 45)
+			robot.go(20,0)
+		if(current == "sing"):
+			robot.playSong( [(88,8),(88,16),(88,16),(84,8),(88,16),(91,32),(79,16)] )
 
-class Keeper:
-	timer = 20;
-	def keepsafe(self, robot):
-		while True:
-			sensors = robot.sensors([create.LEFT_BUMP, create.RIGHT_BUMP, create.CLIFF_FRONT_LEFT, create.CLIFF_FRONT_RIGHT, create.CLIFF_RIGHT, create.CLIFF_LEFT])
-			if (sensors[create.LEFT_BUMP] == 1) or (sensors[create.RIGHT_BUMP] == 1) or (sensors[create.CLIFF_FRONT_LEFT] == 1) or (sensors[create.CLIFF_FRONT_RIGHT] == 1) or (sensors[create.CLIFF_RIGHT] == 1) or (sensors[create.CLIFF_LEFT] == 1):
-				robot.stop()
-				timer = 20
-			elif (timer == 0):
-				robot.stop()
-				timer = 20
-			else:
-				timer = timer - 1
+
 
 
